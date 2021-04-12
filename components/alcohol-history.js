b88fdb5167db330e0f5d70265c2ea1ee4acd65f5
Jesse Zahrt <jdzahrt@gmail.com>
2021-04-12 10:11:17 -0500

    Deltes working and more refactoring

--- components/alcohol-history.js
+++ components/alcohol-history.js
@@ -1,8 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import moment from 'moment';
+import styles from '../styles/Home.module.css';
+
+const defaultDate = new Date().toISOString().substr(0, 10);
 
 const AlcoholHistory = () => {
     const [alcoholHistory, setAlcoholHistory] = useState([])
+    const [quitDate, setQuitDate] = useState([])
 
     const fetchAlcoholHistory = async () => {
         const data = await fetch(`/api/alcohol-tracker/get-history`);
@@ -11,7 +15,6 @@ const AlcoholHistory = () => {
     }
 
     useEffect(() => {
-        console.log('alcoholHistory', alcoholHistory);
         fetchAlcoholHistory();
     }, [])
 
@@ -24,26 +27,66 @@ const AlcoholHistory = () => {
         await fetchAlcoholHistory();
     }
 
+    const handleDateChange = (event) => {
+        setQuitDate(event.target.value)
+    }
+
     const calcDaysQuit = (quitDate) => {
         const date = new Date(quitDate)
         const currentDate = new Date()
+        console.log('date', date);
+        console.log('currentDate', currentDate);
         const timeDiff = currentDate.getTime() - date.getTime()
+        console.log('timeDiff', timeDiff);
         const daysDiff = timeDiff / (1000 * 3600 * 24);
+        console.log('daysDiff', daysDiff);
         const daysDiffRounded = Math.round(daysDiff * 100) / 100
-
+        console.log('daysDiffRounded', daysDiffRounded);
         return daysDiffRounded
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        await fetch('/api/alcohol-tracker/add',
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'applicaiton/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    quitDate
+                })
+            })
+            .then(res => res.json())
+            .catch(error => console.log(error))
+            .then(response => console.log('Success', response))
+
+        await fetchAlcoholHistory();
     }
 
     return (
         <div>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.card}>
+                    <label>
+                        Quit Date:
+                        <input type="date" defaultValue={defaultDate} onChange={handleDateChange}/>
+                    </label>
+                    <br/>
+                    <input type="submit" value="Save"/>
+                </div>
+            </form>
             <center><h1>Alcohol History List</h1></center>
             {alcoholHistory.map((record) => (
                 <ul key={record._id}>
                     <p>
-                        Quit on {moment(record.quitDate).format('MM-DD-YYYY')}.... {calcDaysQuit(record.quitDate)} Days Alcohol
+                        Quit on {moment(record.quitDate).format('MM-DD-YYYY')}
+                        .... {calcDaysQuit(moment(record.quitDate))} Days
+                        Alcohol
                         FREE! 🍻
                     </p>
-                    {/*<AlcoholClock quitDate={record.quitDate}/>*/}
                     <button value={record._id} onClick={e => handleStop(e.target.value)}>STOP TRACKING</button>
                     <button value={record._id} onClick={e => handleDelete(e.target.value)}>DELETE RECORD</button>
                 </ul>
--- pages/alcohol-tracker.js
+++ pages/alcohol-tracker.js
@@ -1,44 +1,18 @@
-import {useState, useEffect, useRef} from 'react'
-import AlcoholForm from '../components/alcohol';
 import styles from '../styles/Home.module.css';
 import AlcoholHistory from '../components/alcohol-history';
 
-export default function AlcoholTracker() {
-    // const [alcoholHistory, setAlcoholHistory] = useState([])
-
-    // console.log('here');
-    // useEffect(() => {
-    //     fetch(`/api/alcohol-tracker/history`)
-    //         .then(res => res.json())
-    //         .then(records => {
-    //             setAlcoholHistory(records);
-    //         })
-    //         .catch(error => console.log(error));
-    // }, []);
-    // const fetchAlcoholHistory = async () => {
-    //     const data = await fetch(`/api/alcohol-tracker/history`);
-    //     const results = await data.json();
-    //     setAlcoholHistory(results)
-    // }
-    //
-    // useEffect(() => {
-    //     console.log('alcoholHistory', alcoholHistory);
-    //     fetchAlcoholHistory();
-    // }, [])
-    
+const AlcoholTracker = () => {
     return (
         <div className={styles.container}>
             <main>
                 <h1 className={styles.title}>
                     Lets track how long you have gone without a drank!
                 </h1>
-
-                <div className={styles.grid}>
-                    <AlcoholForm/>
-                </div>
                 <AlcoholHistory/>
                 <a href={'/'} className={styles.footer}>Home</a>
             </main>
         </div>
     )
 }
+
+export default AlcoholTracker
