2f42948b318483b550fccfda666f6dec7a79f592
jdzahrt <jdzahrt@gmail.com>
2022-07-25 21:38:38 -0500

    Next

--- components/vacation.js
+++ components/vacation.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import moment from 'moment';
 import styles from '../styles/Home.module.css';
+import {calcDaysQuit} from "../utils/days";
 
 const Vacation = () => {
-    const calcDaysDiff = (quitDate, endDate) => {
-        quitDate = moment(quitDate)
-        endDate = endDate ? moment(endDate) : null;
-        const currentDate = moment()
-
-        return endDate ? endDate.diff(quitDate, 'days') : currentDate.diff(quitDate, 'days');
-    }
-
     return (
         <div>
         <h1 className={styles.title}>
-            {calcDaysDiff(new Date().toISOString(), '6/12/2022')} days til' vacation 🌴
+            {calcDaysQuit(new Date().toISOString(), '6/12/2022')} days til' vacation 🌴
         </h1>
         </div>
     );
