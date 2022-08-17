f025ba2194f95cd3c0166244d69db7821c95984b
jdzahrt <jdzahrt@gmail.com>
2022-08-17 06:18:45 -0500

    Babel setup

--- tests/services/alcohol.test.js
+++ tests/services/alcohol.test.js
@@ -1,8 +1,22 @@
+import * as fetch from '../../utils/fetch-api';
+import { getAlcoholHistory } from '../../services/alcohol';
 // import { getAlcoholHistory } from '../../services/alcohol';
-// import { fetchApi } from '../../utils/fetch-api';
 
-test('test', () => {
-  // jest.mock(fetchApi);
-  // getAlcoholHistory();
-  expect(1).toEqual(1);
+jest.mock('../../utils/fetch-api', () => ({
+  fetch: jest.fn(),
+}));
+
+// test('test', () => {
+//   // eslint-disable-next-line no-import-assign
+//
+//   // jest.mock(fetchApi);
+//   gets.getAlcoholHistory();
+//   expect(1).toEqual(1);
+// });
+
+describe('TEST', () => {
+  it('calls stuff', async () => {
+    await getAlcoholHistory();
+    expect(fetch.fetchApi).toHaveBeenCalled();
+  });
 });
