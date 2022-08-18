0fe7efee727d39aa96095e99174edb487fe8b56d
Jesse Zahrt <jdzahrt@gmail.com>
2022-08-17 20:15:29 -0500

    secrets to client (#38)

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  env: {
+    PINO_KEY: process.env.PINO_KEY,
+    PINO_TOKEN: process.env.PINO_TOKEN,
+  },
 };
