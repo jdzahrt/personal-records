6d4ae17f2a60cc678efd6bbeee047a0588ef364a
jdzahrt <jdzahrt@gmail.com>
2022-08-22 21:49:21 -0500

    Add _id to be uuid

--- components/workout-history.js
+++ components/workout-history.js
@@ -1,3 +1,4 @@
+import { v4 } from 'uuid';
 import MaterialTable from '@material-table/core';
 
 import { useState, useEffect } from 'react';
@@ -82,11 +83,16 @@ function WorkoutHistory() {
       options={options}
       editable={{
         onRowAdd: (newData) => new Promise((resolve) => {
-          const fullWorkoutHistory = [...workoutData, newData];
+          const payload = {
+            ...newData,
+            _id: v4(),
+          };
+
+          const fullWorkoutHistory = [...workoutData, payload];
           fullWorkoutHistory.sort((a, b) => new Date(b.date) - new Date(a.date));
           setWorkoutHistory(fullWorkoutHistory);
 
-          addWorkout(newData)
+          addWorkout(payload)
             .catch((error) => logger.error(error));
 
           resolve();
