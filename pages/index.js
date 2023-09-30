2a59967741b5bbe390ae1764fff2e54ac0f78841
jdzahrt <jdzahrt@gmail.com>
2023-09-30 13:55:29 -0500

    Handle login, remove .idea

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Link } from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
 import styles from '../styles/Home.module.css';
 import Header from '../components/header';
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <div className={styles.container}>
-      <Header />
       <Head>
         <title>Personal Records Tracker</title>
       </Head>
@@ -16,33 +17,30 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Personal Records Tracker!
         </h1>
+        {session ? (
+          <div className={styles.grid}>
+            <a href="/alcohol-tracker" className={styles.card}>
+              <h3>Alcohol Tracker &rarr;</h3>
+              <p>Take you to the page to track your drinks 🍻</p>
+            </a>
 
-        <div className={styles.grid}>
-          <a href="/alcohol-tracker" className={styles.card}>
-            <h3>Alcohol Tracker &rarr;</h3>
-            <p>Take you to the page to track your drinks 🍻</p>
-          </a>
+            <a href="/fast-food-tracker" className={styles.card}>
+              <h3>Fast Food Tracker &rarr;</h3>
+              <p>Take you to the page to track your fast food habits 🍔</p>
+            </a>
 
-          {/* <a href="/workout-tracker" className={styles.card}> */}
-          {/*   <h3>Workout Personal Records &rarr;</h3> */}
-          {/*   <p>Take you to the page to track your personal workout records 💪</p> */}
-          {/* </a> */}
+            <a href="/workouts-tracker" className={styles.card}>
+              <h3>Workouts &rarr;</h3>
+              <p>All of your workouts in one location 🏋️</p>
+            </a>
 
-          <a href="/fast-food-tracker" className={styles.card}>
-            <h3>Fast Food Tracker &rarr;</h3>
-            <p>Take you to the page to track your fast food habits 🍔</p>
-          </a>
-
-          <a href="/workouts-tracker" className={styles.card}>
-            <h3>Workouts &rarr;</h3>
-            <p>All of your workouts in one location 🏋️</p>
-          </a>
-
-          <a href="/exercise-tracker" className={styles.card}>
-            <h3>Exercises  &rarr;</h3>
-            <p>All exercises listed in one location 💪</p>
-          </a>
-        </div>
+            <a href="/exercise-tracker" className={styles.card}>
+              <h3>Exercises  &rarr;</h3>
+              <p>All exercises listed in one location 💪</p>
+            </a>
+          </div>
+        ) : <h2>Please sign in to start tracking your personal records!</h2>}
+        <Header />
       </main>
       <footer className={styles.footer}>
         <Link
